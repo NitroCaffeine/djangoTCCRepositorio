1a8eaa47fec9d2b26e71771fd6d67aebbc99dec6
beatriz de souza meneses <beatriz.workspace@gmail.com>
2022-08-27 23:06:14 -0300

    Co-authored-by: Lenner Coutinho Pereira <coutinholenner@gmail.com>

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,32 +3,31 @@ from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
-from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
 
 from .forms import UserForm
 
-
 # Create your views here.
+@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)  
-            messages.success(request, 'Your password was successfully updated!')
             return redirect('tcc:home')
-        else:
-            messages.error(request, 'Please correct the error below.')
     else:
         form = PasswordChangeForm(request.user)
     return render(request, 'mudar_senha.html', {
         'form': form
     })
 
+@login_required
 def listar(request):
     users = User.objects.all()
     return render(request, 'listar_users.html', context={'users': users})
 
+@login_required
 def criar(request):
     if request.method == "POST": 
         form = UserForm(request.POST, request.FILES)  
@@ -41,7 +40,7 @@ def criar(request):
         form = UserForm()  
     return render(request,'criar.html',{'form':form})  
 
-
+@login_required
 def atualizar(request, id):
     user = User.objects.get(id=id)
     form = UserForm(initial={'username': user.username,'last_name': user.last_name, 'email': user.email})
@@ -55,7 +54,7 @@ def atualizar(request, id):
             except Exception as e: 
                 pass    
     return render(request,'atualizar.html',{'form':form})
-
+@login_required
 def deletar(request, id):
     user = User.objects.get(id=id)
     try:
@@ -64,8 +63,10 @@ def deletar(request, id):
         pass
     return redirect('tcc:home')
 
+@login_required
 def logout(request):
     return render(request, 'userlogin.html')
 
+@login_required
 def logoutTrue(request):
     return redirect(request, 'tcc:home')
